59f8189db445be88b5e2b9ca9cb9c5ece2033498
Beyza <beyza.gurer@stu.fsm.edu.tr>
2022-09-20 20:12:38 +0300

    Add note and timer component

---
+++ e-wall/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Timer from "./components/Timer";
+import Note from "./components/Note";
+function App() {
+  return (
+    <div className="App">
+        lets go
+        <Timer/>
+        <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ e-wall/src/components/Timer.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+const Timer = () => {
+
+    const [hours, setHours] = useState(1)
+    const [minutes, setMinutes] = useState(1)
+    const [seconds, setSeconds] = useState(0)
+    
+    useEffect(() => {
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1)
+            }
+            if( seconds === 0){
+                if(minutes === 0){
+                    if(hours === 0){
+                        clearInterval(myInterval)
+                    }else{
+                        setHours(hours - 1)
+                        setMinutes(59)
+                        setSeconds(59)
+                    }
+                }else{
+                    setMinutes(minutes - 1)
+                    setSeconds(59)
+                }
+            }
+        }, 1000)
+        return () => {clearInterval(myInterval)}
+    })
+
+  return (
+    <div className='container'>
+        <h1>{hours >  9 ? hours : "0" + hours}:{minutes > 9 ? minutes : "0"+ minutes}:{seconds > 9 ? seconds : "0" + seconds}</h1>
+    </div>
+  )
+}
+
+export default Timer
